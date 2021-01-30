e61672937493e776da9277b9dada0fac3145a00e
Tara Dia <taracdia@gmail.com>
2021-01-30 07:55:22 -0600

    First commit after massive reworking

---
+++ README.md
@@ -0,0 +1,9 @@
+# snipe-bot
+
+This bot will help me get shifts at my job, which has a first come first serve policy for getting shifts
+
+To set up:
+
+Download most current ChromeDriver and put in the same folder
+
+Create a config.py with username and password and profile location defined
---
+++ bot.py
@@ -0,0 +1,86 @@
+import config
+import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+options = webdriver.ChromeOptions()
+# Using profile data allows the user to bypass the two step verification
+options.add_argument("user-data-dir=" + config.location)
+# The screen needs to be maximized so that the filter options are visible on the page so they can be interacted with
+options.add_argument("--start-maximized")
+driver = webdriver.Chrome(options=options)
+driver.get("http://atoz.amazon.work/")
+
+# --------Basic log in start------------
+# Wrapping this in a try-catch block because if the user exits the program by closing the terminal window rather than the chrome window, the browser will still be logged in next time the program is run and trying to find the missing login component will crash the program if the exception is not handled
+try:
+    WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.NAME, "login"))).send_keys(config.username)
+    driver.find_element_by_name("password").send_keys(config.password)
+    driver.find_element_by_name("SubmitButton").send_keys(Keys.ENTER)
+except:
+    print("You are already logged in, no further action is needed")
+# --------Basic log in finished------------
+# Implicit wait will not be used because there are some conditions that need to be checked immediately and need to fail immediately if they are going to fail
+wait = WebDriverWait(driver, 10)
+
+# Click "Find Shifts"
+wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "bottom-right"))).click()
+
+# Set appropriate hours as a filter
+wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@aria-label='Start time']"))).send_keys("0900a")
+wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@aria-label='End time']"))).send_keys("0500p")
+for i in range(10):
+    # Wait for loading element to appear and disappear
+    try:
+        spinner_wrapper = "//div[@class='atoz-spinner-wrapper']"
+        WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH, spinner_wrapper)))
+        WebDriverWait(driver, 30).until_not(EC.presence_of_element_located((By.XPATH, spinner_wrapper)))
+    except:
+        print("No spinner")
+
+    today = datetime.date.today()
+    dayOffset = 2
+    while (dayOffset < 14):
+        firstDay = today + datetime.timedelta(days = dayOffset)
+        shiftDate = firstDay.strftime("%Y-%m-%d")
+        wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@id='daybutton-" + shiftDate + "']"))).click()
+        try:
+            # Try to add a shift by clicking "add" button. If there is no "add" button, go to the next day and try again
+            driver.find_element(By.CLASS_NAME, "btn-add").click()
+            try:
+                # If someone else got the shift first, click the "Ok" button and get back to trying to get a shift. Do NOT increment the dayOffset counter so that the program tries again to get a shift on the same day
+                driver.find_element(By.CLASS_NAME, "btn-primary").click()
+            except:
+                print("Got a shift at: ", shiftDate)
+            finally:
+                # Wait for loading element to appear and disappear
+                TOAST_WRAPPER = "//div[@class='toast-wrapper']"
+                WebDriverWait(driver, 5
+                    ).until(EC.presence_of_element_located((By.XPATH, TOAST_WRAPPER)))
+                WebDriverWait(driver, 30
+                    ).until_not(EC.presence_of_element_located((By.XPATH, TOAST_WRAPPER)))
+        except:
+            dayOffset += 1
+            # When we've looked at the first week, click to the next week. This ensures that the next week's "daybutton"s are available to be clicked
+            if (dayOffset == 7):
+                driver.execute_script("arguments[0].click();", driver.find_element(By.CLASS_NAME, "right"))
+    driver.refresh()
+
+# Tried to get shift but the loading spinner for "adding shift" took too long so shift gone then "sorry no shift" appears and then there is spinner again and then supposed to resume search but doesn't
+
+# raceback (most recent call last):
+#   File "bot.py", line 49, in <module>
+#     wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@id='daybutton-" + shiftDate + "']"))).click()
+#   File "C:\Users\tarac\AppData\Local\Programs\Python\Python37-32\lib\site-packages\selenium\webdriver\remote\webelement.py", line 80, in click
+#     self._execute(Command.CLICK_ELEMENT)
+#   File "C:\Users\tarac\AppData\Local\Programs\Python\Python37-32\lib\site-packages\selenium\webdriver\remote\webelement.py", line 633, in _execute
+#     return self._parent.execute(command, params)
+#   File "C:\Users\tarac\AppData\Local\Programs\Python\Python37-32\lib\site-packages\selenium\webdriver\remote\webdriver.py", line 321, in execute
+#     self.error_handler.check_response(response)
+#   File "C:\Users\tarac\AppData\Local\Programs\Python\Python37-32\lib\site-packages\selenium\webdriver\remote\errorhandler.py", line 242, in check_response
+#     raise exception_class(message, screen, stacktrace)
+# selenium.common.exceptions.ElementClickInterceptedException: Message: element click intercepted: Element <button id="daybutton-2021-01-27" data-testid="dayButton" class="dayButton btn btn-outline-primary" value="Wed Jan 27" role="tab" tabindex="0" aria-label="Wednesday, Jan 27. 2 shifts available" alt-text="Wednesday, Jan 27. 2 shifts available ">...</button> is not clickable at point (768, 131). Other element would receive the click: <div class="api-error-modal-header">...</div>
+#   (Session info: chrome=87.0.4280.141)
